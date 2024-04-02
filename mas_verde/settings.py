e7b3f9b1faa1c25e02025b72a00efe6dff3518ec
dalemanr <jose.aleman7@misena.edu.co>
2024-04-01 21:27:47 -0500

    se agrega el registro de perdidas y generacion de los reportes

--- mas_verde/settings.py
+++ mas_verde/settings.py
@@ -167,5 +167,5 @@ EMAIL_USE_TLS = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-ALLOWED_HOSTS = ['0018-186-31-209-224.ngrok-free.app', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://0018-186-31-209-224.ngrok-free.app', 'https://*.127.0.0.1']
+ALLOWED_HOSTS = ['f187-186-155-17-9.ngrok-free.app', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://f187-186-155-17-9.ngrok-free.app', 'https://*.127.0.0.1']
--- send_email/forms.py
+++ send_email/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.forms import ModelForm
 
+from django import forms
+
 class FormularioContacto(forms.Form):
-    
-    asunto=forms.CharField(label='Asunto', required=True, max_length=30 )
-    contenido=forms.CharField(label='Contenido', max_length=400, widget=forms.Textarea )
+    asunto = forms.CharField(label='Asunto', required=True, max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    contenido = forms.CharField(label='Contenido', max_length=400, widget=forms.Textarea(attrs={'class': 'form-control'}))
